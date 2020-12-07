b24d5281f899d6f4692c38dd8bdea65b0bf2475c
Jim Lawless <lawless.jim@gmail.com>
2020-12-07 06:07:09 -0600

    Add files via upload

---
+++ Day_7/puzzle7.py
@@ -0,0 +1,31 @@
+# Jim Lawless
+# License: https://github.com/jimlawless/AoC2020/LICENSE
+
+rules=[]
+used=""
+
+def searchRules(srch):
+    global rules
+    global used
+    count=0
+    for rule in rules:
+        if rule.find(srch) >= 0:
+            contain=rule.find(" bags contain")
+            has=rule.find(srch)
+            if has > contain:
+                bagtype=rule[0:contain]
+                if used.find(bagtype)<0:
+                    count = count + 1
+                    used=used+"|"+bagtype   
+                    count=count+searchRules(bagtype)
+    return count
+
+
+infile = open("input.txt","r")
+total=0
+for line in infile:
+    line=line.rstrip()
+    rules.insert(0,line)
+infile.close()
+
+print(searchRules("shiny gold"))
